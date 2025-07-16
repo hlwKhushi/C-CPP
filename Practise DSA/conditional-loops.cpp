c90470021c03bcdfb031462d771539418c0ff2ef
khushihappy12 <mekhushikumari200312@gmail.com>
2025-07-16 11:03:01 +0530

    add

--- Practise DSA/conditional-loops.cpp
+++ Practise DSA/conditional-loops.cpp
@@ -1,10 +1,42 @@
 
 //Conditionsl Loops::::::::::::
-/**/
+/*
 #include <iostream>
 using namespace std;
 int main(){
     int n;
-    cin >> n;
+    cin >> n; // Take input in it.
     cout << "Dilkhush" << endl;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cin >> n; //enter any value print as it is.
+  cout << "Value of n is: " << n << endl;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+    if (a>=0){
+        cout << "a is Possitive: " << endl;
+    }
+    else {
+        cout << "a is Negative: " << endl;
+    
+    }
+}
+*/
+
+// Note: 'cin' don't read [space(-), tab(\t), enter(\n)]
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    a = cin.get();
+    cout << "Value of a is: " << a << endl;
 }
